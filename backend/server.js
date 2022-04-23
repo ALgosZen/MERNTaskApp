b70c2600834b16caf14b9c0a24a08c2a83f8d502
Suresh Narayan <goldbook@Sureshs-Air.home>
2022-04-23 08:46:49 -0700

    commit backend code with asynchandler

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const { errorHandler } = require('./handlers/errorHandler')
 const app = express()
 
 const dotenv = require('dotenv').config
 const port = process.env.PORT || 5001
+
+//following two lines json and urlencoded should be set first before the route
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routers/goalRoutes'))
+
+// this handler must be after the route as placed above
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`server listening on port : ${port}`))
